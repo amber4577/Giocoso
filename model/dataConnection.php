3ee98336d9df4dc9fa4fd1e0884c6d7c45583dba
muggleseph <103576011+muggleseph@users.noreply.github.com>
2023-05-24 16:45:56 +0200

    connexion bd

---
+++ model/dataConnection.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'config.php';
+
+function getDatabaseConnectionMySQL(): PDO
+{
+    $dsn = 'mysql/dbname=giocoso:host=' . DB_HOST . ";charset=utf8";
+
+    try {
+        $databaseConnection = new PDO($dsn, DB_USER, DB_PASSWORD);
+        $databaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $databaseConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch (PDOException $e){
+        die('Erreur de connexion à la base de données : ' . $e->getMessage());
+    }
+    
+    // Le PDO s'est connecté à la base de donnée sauf qu'on veut pas qu'il se supprime avec la fonction, on veut l'utiliser plusieurs fois
+    return $databaseConnection;
+}
+
+
+
