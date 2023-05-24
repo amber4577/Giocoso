35bfc47636ad3a621f61965f9d0728d33541021c
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-24 17:46:43 +0200

    nav

---
+++ app/view/common/nav.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>navigation</title>
+    <link rel="stylesheet" href="stylegiocoso.css">
+</head>
+<nav class="menu">
+        <ul>
+            <li><a href="brasserie.html"#brasserie>LA BRASSERIE</a></li>
+            <li><a href="catalogue.html"#catalogue>BIERES</a></li>
+            <li><a href="entreprise.html"#entreprise>NOUS</a></li>
+            <li><a href="contact.html"#contact>CONTACT</a></li>
+            <li><a href="connexion.html"#connexion><img src="iconeprofil.png"/></a></li>
+            <li><a href="panier.html"#connexion><img src="iconepanier.png"/></a></li>
+        </ul>
+    </nav>
