7bbdd0747cf5107b9849a2fc13885247d8044ef8
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-27 23:20:30 +0200

    update

---
+++ app/view/common/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Le Footer</title>
+    <link rel="stylesheet" href="public/css/header.css">
+</head>
+<header>
+    <section class="logo">
+        <img src="logo_giocoso.png"/>
+        <img src="public/images/imgheader/logo_giocoso.png"/>
+    </section>
+    <?php include 'app/view/common/nav.php'; ?>
+</header>     
