74d85e6c5e6c454dc460b963f2b8b112c698f8fb
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-27 20:08:59 +0200

    test

--- Client_Connu.php
+++ Client_Connu.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="utf-8">
     <title>Page Client</title>
-    <link rel="stylesheet" href="public/css/Client_Connu.css">
+    <link rel="stylesheet" href="public/css/stylegiocoso.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@700&display=swap" rel="stylesheet">
@@ -11,24 +11,18 @@
     <?php include 'app/view/common/header.php';?>     
 </header>
 <body>
-    <h1 class="MesInfos">Mes informations<h1>
-        <div class="info">
-            <h6 class = police>
-                RENSEIGNER VOTRE NUMÉRO CLIENT
-            </h6>
+    <h2>MES INFORMATIONS</h2>
 
-                     <input type="numéro client"/> <!--C'est pour le numéro client-->
+    <section class="numeroclient">
+            <h3>RENSEIGNER VOTRE NUMERO CLIENT</h3>
+            <input id="barredenum" onkeyup="numero" type="text" name="number">
+            <div id="question">
+                <a href="commandeclientinconnu.php"#commconnu>VOUS N'AVEZ PAS DE NUMERO CLIENT ?</a>
+            </div>
 
-                    <h6 class = police>
-                            <a href="url" class="Compte" style="     /* text-decoration-color: white ; */     text-decoration: none;     color: white; text-decoration: underline;
-                            ">JE N'AI PAS DE NUMÉRO CLIENT
-                            </a>
-                    </h6>
-                    
-                        <button>
-                            <a href="url" class="Num" style="     /* text-decoration-color: white ; */     text-decoration: none;       color: #232323;">
-                                RECUPÉRER MES INFORMATIONS
-                            </a>
-                        </button>       
-        </div>
+            <div id="boutton">
+                <button class="validation" style="color: #232323;">RECUPERER MES INFORMATIONS</button>
+            </div>
+    </section>
 </body>
+
