6a11764194ea5590831aacedb321e0e723a78d62
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-28 20:30:41 +0200

    up

--- NOS BIERES.php
+++ NOS BIERES.php
@@ -30,27 +30,27 @@
     <div class="container">
  
     <li>
-        <img src = "./public/images/bieres/biere_electro.png" alt = "bière electro" style=" width: 70%; background-color: #D11FE3;
+        <img src = "./public/images/bieres/biere_electro.png" alt = "bière electro" style=" width: 7=80%; background-color: #D11FE3;
         border-radius: 2em; height : 300px;">
         <p>ELECTRO         4,20 €</p>
     </li>
     <li>
-        <img src = "./public/images/bieres/biere_funky.png" alt = "bière funky" style=" width: 70%; background-color: #FF398A;
+        <img src = "./public/images/bieres/biere_funky.png" alt = "bière funky" style=" width: 80%; background-color: #FF398A;
         border-radius: 2em; height : 300px;">
         <p>FUNKY         4,20 €</p>
     </li>
     <li>
-        <img src = "./public/images/bieres/biere_jazz.png" alt = "bière jazz" style=" width: 70%; background-color: #ffcb3d;
+        <img src = "./public/images/bieres/biere_jazz.png" alt = "bière jazz" style=" width: 80%; background-color: #ffcb3d;
         border-radius: 2em; height : 300px;">
         <p>JAZZ         4,20 €</p>
     </li>
     <li>
-        <img src = "./public/images/bieres/biere_rock.png" alt = "bière rock" style=" width: 70%; background-color:#A81A16 ;
+        <img src = "./public/images/bieres/biere_rock.png" alt = "bière rock" style=" width: 80%; background-color:#A81A16 ;
        border-radius: 2em; height : 300px;">
        <p>ROCK         4,20 €</p>
     </li>
     <li>
-        <img src = "./public/images/bieres/biere_tango.png" alt = "bière tango" style=" width: 70%; background-color:#EA3434;
+        <img src = "./public/images/bieres/biere_tango.png" alt = "bière tango" style=" width: 80%; background-color:#EA3434;
         border-radius: 2em; height : 300px;">
         <p>TANGO         4,20 €</p>
     </li>
