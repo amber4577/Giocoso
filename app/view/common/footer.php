16a8df879c32e1a97e46ebfe93868ab388254bb0
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-28 13:53:12 +0200

    test

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -11,7 +11,7 @@
         background-image: url("public/images/logo_rs/FIN2.png") ;
         background-repeat: no-repeat;
         position : relative;
-        bottom : 400px;
+        bottom : 800px;
     }
         </style>
     <footer>
