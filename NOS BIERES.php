8a6962f7aa2beea745f64017243fc9556923b51a
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-28 20:07:42 +0200

    up

--- NOS BIERES.php
+++ NOS BIERES.php
@@ -31,22 +31,22 @@
  
     <li>
         <img src = "./public/images/bieres/biere_electro.png" alt = "bière electro" style=" width: 80%; background-color: #D11FE3;
-        border-radius: 41px; height : 90px;">
+        border-radius: 41px; height : 300px;">
     </li>
     <li>
         <img src = "./public/images/bieres/biere_funky.png" alt = "bière funky" style=" width: 80%; background-color: #FF398A;
-        border-radius: 41px; height : 90px;">
+        border-radius: 41px; height : 300px;">
     </li>
     <li>
         <img src = "./public/images/bieres/biere_jazz.png" alt = "bière jazz" style=" width: 80%; background-color: #ffcb3d;
-        border-radius: 41px; height : 90px;">
+        border-radius: 41px; height : 300px;">
     </li>
     <li>
         <img src = "./public/images/bieres/biere_rock.png" alt = "bière rock" style=" width: 80%; background-color:#A81A16 ;
-        border-radius: 41px; height : 90px;">
+        border-radius: 41px; height : 300px;">
     </li>
     <li>
         <img src = "./public/images/bieres/biere_tango.png" alt = "bière tango" style=" width: 80%; background-color:#EA3434;
-        border-radius: 41px; height : 90px;">
+        border-radius: 41px; height : 300px;">
     </li>
 </div>
