593c19d575e77e79b2c796ba4ff476d83dda553a
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-29 14:15:41 +0200

    up

--- publicite.php
+++ publicite.php
@@ -4,14 +4,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Accueil</title>
-    <link rel="stylesheet" href="public/css/stylegiocoso.css">
+    <link rel="stylesheet" href="public/css/publicite.css">
 </head>
 
 <body>
-    <section class="publicite">
+    <div class="home">
         <video controls loop autoplay>
             <source src="public/video/pub.mp4" type="video/mp4">
         </video>
-    </section>
+    </div>
 
 </body>
