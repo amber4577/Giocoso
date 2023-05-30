6b1c127f6e1d512b92ec82f459329cd8d68a4b77
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-30 11:08:01 +0200

    up

--- publicite.php
+++ publicite.php
@@ -9,6 +9,6 @@
 
 <body>
     <div class="home">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/TCvJBkxwasU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://youtu.be/h_TLhlCYiVQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 </body>
