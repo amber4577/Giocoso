255720ce9d26edcb7b0ad0a717501c34ba5ff80f
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-05-30 11:12:31 +0200

    up

--- accueil.php
+++ accueil.php
@@ -21,7 +21,7 @@
         </div>
 
         <section class="publicite">
-        <?php include 'publicite.php';?> 
+            <iframe width="560" height="315" src="https://youtu.be/h_TLhlCYiVQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </section>
 
         <section class="gamme">
