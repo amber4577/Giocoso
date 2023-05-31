203f2c7b88fcf2c3b7fb3b0255ff733abf606458
Chaylis <salhi.chaim4@gmail.com>
2023-05-31 12:21:51 +0200

    nosBiere.view

--- NOS_BIERES.php
+++ NOS_BIERES.php
@@ -3,5 +3,5 @@
 ob_start();
 require_once 'app/view/NosBieres.view.php';
 $content = ob_get_clean();
-require_once 'app/model/view/commun/layout.php'; 
+require_once 'app/view/common/layout.php'; 
 
