ec509e19c3640f64f08b5e1a78a534b07d6e6be6
Chaylis <salhi.chaim4@gmail.com>
2023-05-31 11:08:37 +0200

    nosBiere.view.php

--- NOS BIERES.php
+++ NOS BIERES.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-
-<head>
-    <meta charset="utf-8">
-    <title>Nos bières</title>
-    <link rel="stylesheet" href=".app/public/css/NosBières.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@700&display=swap" rel="stylesheet">
-
-</head>
-
 <header>
     <?php include 'app/view/common/header.php'; ?>
 </header>
