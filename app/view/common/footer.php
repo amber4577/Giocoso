73de956b08a32e88c6b1e88fe803bb15b275e71d
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-01 21:28:57 +0200

    up

--- app/view/common/footer.php
+++ app/view/common/footer.php
@@ -48,15 +48,15 @@
 
         <div class="RS">
             <a href="url">
-                <img src="public/image/facebook.png" alt="facebook">
+                <img src="public/images/facebook.png" alt="facebook">
             </a>
 
             <a href="url">
-            <img src="public/image/twitter.png" alt="twitter">
+                <img src="public/images/twitter.png" alt="twitter">
             </a>
             
             <a href="url">
-            <img src="public/image/instagram.png" alt="instagram">
+                <img src="public/images/instagram.png" alt="instagram">
             </a>
         </div>
 
