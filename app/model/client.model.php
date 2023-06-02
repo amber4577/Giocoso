ff918c42d0bcdb4c76779fd6485e00c8688baab9
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-02 17:45:41 +0200

    up

--- app/model/client.model.php
+++ app/model/client.model.php
@@ -9,3 +9,26 @@ function getDoneClient(int $identifiant, PDO $db): array
     $stmt->execute();
     return $stmt->fetchAll();
 }
+
+
+function addClient(array $client, PDO $db): bool
+{
+
+    try {
+        $sql = 'INSERT INTO client (nom, prenom, adresse_mail, num_tel, adresse_postale, date_naissance, cp, ville)
+                VALUES (:nom, :prenom, :adresse_mail, :num_tel, :adresse_postalen :date_naissance, :cp, :ville)';
+
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue('nom', $client['nom']);
+        $stmt->bindValue('prenom', $client['prenom']);
+        $stmt->bindValue('adresse_mail', $client['adresse_mail']);
+        $stmt->bindValue('num_tel', $client['num_tel']);
+        $stmt->bindValue('adresse_posrale', $client['adresse_postale']);
+        $stmt->bindValue('date_naissance', $client['date_naissance']);
+        $stmt->bindValue('cp', $client['cp']);
+        $stmt->bindValue('ville', $client['ville']);
+        return $stmt->execute();
+    } catch (PDOException $e) {
+        throw $e;
+    }
+}
--- commandeclientinconnu.php
+++ commandeclientinconnu.php
@@ -1,28 +1,12 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8"/>
-    <title>Commande</title>
-    <link rel="stylesheet" href="public/css/stylegiocoso.css">
-</head>
-<header>
-    <?php include 'app/view/common/header.php';?>     
-</header>  
-<body>
-    <h2>MES INFORMATIONS</h2>
+<?php
+$page_title = 'Inscription';
+$css = 'stylegiocoso.css';
 
-    <section class="numeroclient">
-            <h3>CREER VOTRE NUMERO CLIENT</h3>
-            <input id="barredenum" onkeyup="numero" type="text" name="number">
-            <div id="question">
-                <a href="Client_Connu.php"#commconnu>AVEZ-VOUS DEJA UN COMPTE ?</a>
-            </div>
+ob_start();
+require_once 'app/view/forminscription.view.php';
+require_once 'app/model/client.model.php';
+$content = ob_get_clean();
+require_once 'app/view/common/layout.php';
 
-            <div id="boutton">
-                <a href="compteclientform.php"><button class="validation" style="background-color: #70F8BA;">CREER MON NUMERO</button></a>
-            </div>
-    </section>
 
-    <footer>
-        <?php include 'app/view/common/footer.php';?>     
-    </footer>
-</body>
+addClient($identifiant, $db);
