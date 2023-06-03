a8d39f091007cbb2d9e9d43ed9752a0b311c3f30
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-04 00:43:14 +0200

    up

--- app/view/commandeclientinconnu.view.php
+++ app/view/commandeclientinconnu.view.php
@@ -5,13 +5,14 @@
             <h3>CREER VOTRE NUMERO CLIENT</h3>
             <input id="barredenum" onkeyup="numero" type="text" name="number">
             
-            <div id="question">
-                <a href="Client_Connu.php"#commconnu>AVEZ-VOUS DEJA UN COMPTE ?</a>
+            <div id="boutton">
+                <a href="app/view/compteclientform.php"><button type="submit" class="validation" style="background-color:#70F8BA;">CREER VOTRE COMPTE</button></a>
             </div>
 
-            <div id="boutton">
-            <a href="compteclientform.php"><button type="submit" class="validation" style="background-color:#70F8BA;">CREER MON COMPTE</button></a>
-    </div>
+        <div id="question">
+             <a href="commandeclientinconnu.php"#commconnu>VOUS AVEZ DEJA UN NUMERO CLIENT ?</a>
+        </div>
+    </section>
             </div>
     </section>
 </body>
