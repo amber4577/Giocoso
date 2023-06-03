299ca6d250e8b6161f5d977d21e52beef90414fe
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-03 23:26:43 +0200

    up

--- compteclientform.php
+++ compteclientform.php
@@ -4,7 +4,6 @@ $css = 'stylegiocoso.css';
 
 ob_start();
 require_once 'app/view/compteclientform.view.php';
-require_once 'app/model/client.model.php';
 require_once 'config.php';
 $content = ob_get_clean();
 require_once 'app/view/common/layout.php';
