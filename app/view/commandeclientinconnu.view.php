bc894421209cbca4864dfe3cbc6039ffd1c565f9
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-04 00:40:40 +0200

    up

---
+++ app/view/commandeclientinconnu.view.php
@@ -0,0 +1,17 @@
+<body>
+    <h2>MES INFORMATIONS</h2>
+
+    <section class="numeroclient">
+            <h3>CREER VOTRE NUMERO CLIENT</h3>
+            <input id="barredenum" onkeyup="numero" type="text" name="number">
+            
+            <div id="question">
+                <a href="Client_Connu.php"#commconnu>AVEZ-VOUS DEJA UN COMPTE ?</a>
+            </div>
+
+            <div id="boutton">
+            <a href="compteclientform.php"><button type="submit" class="validation" style="background-color:#70F8BA;">CREER MON COMPTE</button></a>
+    </div>
+            </div>
+    </section>
+</body>
--- commandeclientinconnu.php
+++ commandeclientinconnu.php
@@ -1,17 +1,11 @@
-<body>
-    <h2>MES INFORMATIONS</h2>
+<?php
+require_once 'app/model/dataConnection.php';
 
-    <section class="numeroclient">
-            <h3>CREER VOTRE NUMERO CLIENT</h3>
-            <input id="barredenum" onkeyup="numero" type="text" name="number">
-            
-            <div id="question">
-                <a href="Client_Connu.php"#commconnu>AVEZ-VOUS DEJA UN COMPTE ?</a>
-            </div>
 
-            <div id="boutton">
-            <a href="compteclientform.php"><button type="submit" class="validation" style="background-color:#70F8BA;">CREER MON COMPTE</button></a>
-    </div>
-            </div>
-    </section>
-</body>
+$page_title = 'Inscription';
+$css = 'stylegiocoso.css';
+
+ob_start();
+require_once 'app/view/commandeclientinconnu.view.php';
+$content = ob_get_clean();
+require_once 'app/view/common/layout.php'; 
