d3207762f50dfdfa9ec2a8e512ad943b5c34da36
amber4577 <ambre.wery.aw@gmail.com>
2023-06-05 18:26:42 +0200

    brassage

--- app/view/common/nav.php
+++ app/view/common/nav.php
@@ -1,8 +1,7 @@
 <nav class="menu">
         <ul>
-            <li><a href="brasserie.php">LA BRASSERIE</a></li>
+            <li><a href="brassage.php">LE BRASSAGE</a></li>
             <li><a href="NOS_BIERES.php">BIERES</a></li>
-            <li><a href="Nous.php">NOUS</a></li>
             <li><a href="compteclientform.php">CONTACT</a></li>
             <li><a href="Client_connu.php"><img src="public/images/imgheader/iconeprofil.png"/></a></li>
             <li><a href="panier.html"><img src="public/images/imgheader/iconepanier.png"/></a></li>
