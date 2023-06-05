c01d61dc257f6d609b15ef27f600f5e61c964a26
muggleseph <103576011+muggleseph@users.noreply.github.com>
2023-06-05 19:56:57 +0200

    update

--- app/view/common/nav.php
+++ app/view/common/nav.php
@@ -3,7 +3,7 @@
             <li><a href="brasserie.php">LA BRASSERIE</a></li>
             <li><a href="NOS_BIERES.php">BIERES</a></li>
             <li><a href="Nous.php">NOUS</a></li>
-            <li><a href="compteclientform.php">CONTACT</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
             <li><a href="Client_connu.php"><img src="public/images/imgheader/iconeprofil.png"/></a></li>
             <li><a href="panier.html"><img src="public/images/imgheader/iconepanier.png"/></a></li>
         </ul>
