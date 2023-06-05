61e133819de57e1efc4831d500556998ee3cd27f
amber4577 <ambre.wery.aw@gmail.com>
2023-06-05 19:56:06 +0200

    Update nav.php

--- app/view/common/nav.php
+++ app/view/common/nav.php
@@ -2,7 +2,7 @@
         <ul>
             <li><a href="brassage.php">BRASSAGE</a></li>
             <li><a href="NOS_BIERES.php">BIERES</a></li>
-            <li><a href="compteclientform.php">CONTACT</a></li>
+            <li><a href="contact.php">CONTACT</a></li>
             <li><a href="Client_Connu.php"><img src="public/images/imgheader/iconeprofil.png"/></a></li>
         </ul>
     </nav>
