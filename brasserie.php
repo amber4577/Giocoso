afebe4e3c8d804bb634679061e62373f50e33671
amber4577 <ambre.wery.aw@gmail.com>
2023-06-05 12:03:53 +0200

    modif

---
+++ brasserie.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Brasserie</title>
+    <link rel="stylesheet" href="public/css/stylegiocoso.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    
+</body>
