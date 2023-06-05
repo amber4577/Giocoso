c7f0be21bc6224e85eb26be290c7bd67e5d88647
amber4577 <ambre.wery.aw@gmail.com>
2023-06-05 18:30:55 +0200

    change panier

--- app/view/common/nav.php
+++ app/view/common/nav.php
@@ -4,6 +4,5 @@
             <li><a href="NOS_BIERES.php">BIERES</a></li>
             <li><a href="compteclientform.php">CONTACT</a></li>
             <li><a href="Client_connu.php"><img src="public/images/imgheader/iconeprofil.png"/></a></li>
-            <li><a href="panier.html"><img src="public/images/imgheader/iconepanier.png"/></a></li>
         </ul>
     </nav>
