3ae7f26a8dcbf1977e09660b949372cb0986809b
muggleseph <103576011+muggleseph@users.noreply.github.com>
2023-06-05 18:24:09 +0200

    brassage changemnets

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -3,6 +3,6 @@
 <p>Voici comment s'est déroulé le brassage de la bière Funky et ses caractéristiques.</p>
 
 <section class="vulgarisation">
-    <img src="public/images/vulgarisation.png"/></a>
+    <img id="image" src="public/images/vulgarisation.png"/></a>
 </section>
     
