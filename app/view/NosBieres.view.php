515e469d5e20356bc5b2dddfd35e107d74b6370a
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-07 07:36:03 +0200

    up

--- app/view/NosBieres.view.php
+++ app/view/NosBieres.view.php
@@ -1,25 +1,33 @@
-<style>
-        .grid-container {
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
-            grid-gap: 10px;
-        }
-    </style>
-<div class="grid-container">
-    <?php
+<section class="gamme">
+            <?php include 'presentationgamme.php';?> 
+</section>
 
-        $sql = "SELECT nom_img FROM bière";
-        $resultat = mysqli_query($databaseConnection, $sql);
 
-        while ($ligne = mysqli_fetch_assoc($resultat)) {
-            $nomIMg = $ligne['nom_img'];
-            echo "<div class='grid-item'> <ul> 
-            <li><img src='public/images/repertoire/ $nomIMg' alt='$nomIMg'></li>;
-            <li><img src='public/images/repertoire/ $nomIMg' alt='$nomIMg'></li>;
-            <li><img src='public/images/repertoire/ $nomIMg' alt='$nomIMg'></li>;
-            <li><img src='public/images/repertoire/ $nomIMg' alt='$nomIMg'></li>;
-            <li><img src='public/images/repertoire/ $nomIMg' alt='$nomIMg'></li> 
-            </ul>";
-        }
-    ?>
+<div class="container">
+ 
+ <li>
+     <a href="biereElectro.php"><img src = "./public/images/bieres/biere_electro.png" alt = "bière electro" style=" width: 80%; background-color: #D11FE3;
+     border-radius: 2em; height : 300px;"></a>
+     <p>ELECTRO         4,20 €</p>
+ </li>
+ <li>
+ <a href="biereFunky.php"><img src = "./public/images/bieres/biere_funky.png" alt = "bière funky" style=" width: 80%; background-color: #FF398A;
+     border-radius: 2em; height : 300px;"></a>
+     <p>FUNKY         4,20 €</p>
+ </li>
+ <li>
+ <a href="biereJazz.php"><img src = "./public/images/bieres/biere_jazz.png" alt = "bière jazz" style=" width: 80%; background-color: #ffcb3d;
+     border-radius: 2em; height : 300px;"></a>
+     <p>JAZZ         4,20 €</p>
+ </li>
+ <li>
+ <a href="biereRock.php"> <img src = "./public/images/bieres/biere_rock.png" alt = "bière rock" style=" width: 80%; background-color:#A81A16 ;
+    border-radius: 2em; height : 300px;"></a>
+    <p>ROCK         4,20 €</p>
+ </li>
+ <li>
+ <a href="biereTango.php"><img src = "./public/images/bieres/biere_tango.png" alt = "bière tango" style=" width: 80%; background-color:#EA3434;
+     border-radius: 2em; height : 300px;"></a>
+     <p>TANGO         4,20 €</p>
+ </li>
 </div>
