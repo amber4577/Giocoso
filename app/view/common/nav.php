b9067c31203a98a1465f22374e804b33c0c623ca
rakotok2109 <131179281+Rakotok45Montargis@users.noreply.github.com>
2023-06-07 07:43:03 +0200

    up

--- app/view/common/nav.php
+++ app/view/common/nav.php
@@ -2,7 +2,6 @@
         <ul>
             <li><a href="brassage.php">BRASSAGE</a></li>
             <li><a href="NOS_BIERES.php">BIERES</a></li>
-            <li><a href="Nous.php">NOUS</a></li>
             <li><a href="contact.php">CONTACT</a></li>
             <li><a href="Client_connu.php"><img src="public/images/imgheader/iconeprofil.png"/></a></li>
             <li><a href="panier.html"><img src="public/images/imgheader/iconepanier.png"/></a></li>
